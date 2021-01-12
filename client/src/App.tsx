014becf3555e6526d156d2dea73babaf211cc8e2
jomariepolanco <jomariepolanco@gmail.com>
2021-01-11 19:49:31 -0500

    app layout done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,20 @@ import Navbar from './components/Navbar';
 import NewsContainer from './containers/NewsContainer';
 import NotesContainer from './containers/NotesContainer';
 import 'fontsource-roboto';
+import { createStyles, Grid, makeStyles, Paper, Theme } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) => 
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary
+    }
+  })
+)
 
 function App() {
   const [data, setData] = React.useState("null");
@@ -12,13 +26,23 @@ function App() {
       .then((result) => result.text())
       .then((res) => setData(res));
   };
+
+  const classes = useStyles()
   return (
-    <div className="App">
-        <button onClick={getData}>Click Me For Data</button>
-        {data}
+    <div className={classes.root}>
         <Navbar />
-        <NewsContainer />
-        <NotesContainer />
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <NewsContainer />
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <NotesContainer />
+            </Paper>
+          </Grid>
+        </Grid>
     </div>
   );
 }
