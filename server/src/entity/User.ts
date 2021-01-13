966abe7d025ab272095614341da369e5e75a3b94
jomariepolanco <jomariepolanco@gmail.com>
2021-01-12 23:59:13 -0500

    validation on user name added

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -7,7 +7,7 @@ export class User extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
+    @Column({type: 'varchar', unique: true})
     name: string;
 
     @OneToMany(type => Note, note => note.user)
